9d46600ea9fdf14dd6e8fa59836e85be951d568f
alex-lairan <lairana@free.fr>
2016-07-04 21:03:03 +0200

    Permit to launch a server

--- src/Opali.cr
+++ src/Opali.cr
@@ -1,5 +1,21 @@
+require "http/server"
+require "json"
 require "./Opali/*"
 
 module Opali
-  # TODO Put your code here
+  class Server
+    def initialize(args)
+      @config = Opali::Config.new(args)
+      @server = HTTP::Server.new(@config.port) do |context|
+        context.response.content_type = "application/json"
+        response = { code: 200, status: :ok }
+        context.response.print response.to_json
+      end
+    end
+
+    def start
+      pp @server
+      @server.listen
+    end
+  end
 end
