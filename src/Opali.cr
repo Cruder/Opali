7ed0eb77b4838888e46a84a0df1410f1c5dc372f
alex-lairan <a.lairan@linkbynet.com>
2016-07-05 13:02:22 +0200

    Add timer

--- src/Opali.cr
+++ src/Opali.cr
@@ -8,9 +8,15 @@ module Opali
     def initialize(args)
       @config = Opali::Config.new(args)
       @server = HTTP::Server.new(@config.port) do |context|
+        time_before = Time.now
+
         context.response.content_type = "application/json"
-        response = { code: 200, status: :ok }
+        response = { code: Opali::HTTP::Codes::LIST[:ok], status: :ok,
+                     path: context.request.path }
         context.response.print response.to_json
+
+        puts "Server respond on #{(Time.now - time_before).hash / 10}µs "\
+             "on route \"#{context.request.path}\""
       end
     end
 
